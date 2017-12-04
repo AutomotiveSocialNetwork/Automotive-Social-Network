82d4bcc47d81fac8febc57bc19930cc20e3e873f
nohadrweesh <noha_drweesh@yahoo.com>
2017-12-04 15:35:49 +0200

    gps code v1

--- app/src/main/java/com/example/trackyourlocation/TestPost.java
+++ app/src/main/java/com/example/trackyourlocation/TestPost.java
@@ -166,7 +166,7 @@ public class TestPost extends Activity{
 
         });
 
-        
+
         list = (ListView) findViewById(R.id.list1);
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
