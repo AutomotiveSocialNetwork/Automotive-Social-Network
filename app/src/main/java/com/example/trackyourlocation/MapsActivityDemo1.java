771c4e004e87dcf0dde51eb85bbae92f50963e1e
nohadrweesh <noha_drweesh@yahoo.com>
2017-12-04 16:13:04 +0200

    gps v2

--- app/src/main/java/com/example/trackyourlocation/MapsActivityDemo1.java
+++ app/src/main/java/com/example/trackyourlocation/MapsActivityDemo1.java
@@ -12,7 +12,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -76,8 +75,6 @@ public class MapsActivityDemo1 extends AppCompatActivity implements OnMapReadyCa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_demo1);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
